f96cce1f3c2a8c0e64a22425bc1d0e560427d091
zacharysanders-sketch <zachary.sanders@bsd52.org>
2026-05-05 00:21:53 -0700

    Delete utils.js

--- utils.js
+++
@@ -1,6 +0,0 @@
-function drawCenteredText(ctx, text, font, color, x, y) {
-  ctx.font = font;
-  ctx.fillStyle = color;
-  const metrics = ctx.measureText(text);
-  ctx.fillText(text, x - metrics.width / 2, y);
-}
